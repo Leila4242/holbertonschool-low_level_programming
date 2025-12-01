3a9b0e2e079ce633d952f6594f729183990cb2a4
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-01 12:37:50 +0000

    LAst digit comparing

---
+++ 1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/*Compare and get a result*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n<6 && n!=0){
+        printf("%d and is greater than 5\n",n);
+    }
+    else if(n>5){
+        printf("%d and is less than 6 and not 0",n);
+    }
+    else{
+        printf("%d and is 0",n);
+    }
+	return (0);
+}
