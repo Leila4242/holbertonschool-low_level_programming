8a52ffd74a675d0b9abe6cbf557845063439c10c
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-02 07:42:43 +0000

    Comparing and getting results

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,13 +12,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
     int last = n%10;
 	if(last<6 && last>0){
-        printf("%d and is less than 6 and not 0\n",n);
+        printf("%d and is less than 6 and not 0\n",last);
     }
     else if(last>5){
-        printf("%d and is greater than 5\n",n);
+        printf("%d and is greater than 5\n",last);
     }
     else if(last==0){
-        printf("%d and is 0\n",n);
+        printf("%d and is 0\n",last);
     }
 	return (0);
 }
