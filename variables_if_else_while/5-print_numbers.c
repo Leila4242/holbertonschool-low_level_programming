f7347e2c8ca9d1ef585266fcdedcfd1a3034271d
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-02 11:09:48 +0000

    0-10 printing

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints numbers from 0 to 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 10)
+	{
+		printf("%d\n", c);
+		c++;
+	}
+
+	return (0);
+}
