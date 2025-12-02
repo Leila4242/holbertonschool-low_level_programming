b8f906b6b8c62b035ec800ee33a2527308608967
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-02 08:56:18 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the alphabet in lowercase followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c,ch;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
