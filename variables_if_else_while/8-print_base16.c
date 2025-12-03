71d75c324750ecf1ac2a26345fd691b2669f2d0a
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-03 10:52:03 +0000

    Hexadecimal printing

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * Program to print all hexadecimal (base 16) digits in lowercase
+ */
+
+int main(void)
+{
+    int i;
+    
+    i = 0;
+    // Print all hexadecimal digits (0-9, a-f)
+    while (i < 16)
+    {
+        if (i < 10)
+            putchar(i + '0');
+        else
+            putchar(i - 10 + 'a');
+        i++;
+    }
+    
+    putchar('\n');  // Add newline at the end
+    
+    return (0);
+}
