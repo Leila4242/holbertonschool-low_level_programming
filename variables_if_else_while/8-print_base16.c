02f5e4d534061e46b4e3666d1ca626eac2212e44
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-03 10:59:41 +0000

    Hexadecimal printing

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,16 +7,14 @@
 int main(void)
 {
     int i;
-    
     i = 0;
-    while (i < 16)
-    {
+    while (i < 16){
         if (i < 10)
             putchar(i + '0');
         else
             putchar(i - 10 + 'a');
         i++;
-    }
+        }
     putchar('\n'); 
     return (0);
 }
