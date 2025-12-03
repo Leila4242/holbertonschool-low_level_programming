4101301153e9fa910870dad681d87655c5abfc3a
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-03 11:14:29 +0000

    Printing 0-9 seperated by ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints numbers 0 to 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+	    if(c!=9){
+	        putchar(c + '0');
+		    putchar(',');
+		    putchar(' ');
+		    c++;  
+	    }
+	    else{
+	        putchar(c + '0');
+		    putchar(' ');
+		    c++;
+	    }
+	}
+	putchar('\n');
+
+	return (0);
+}
