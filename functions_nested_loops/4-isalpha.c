4ff7e64fe7501a8a194645290ddaed95b42f248f
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-07 19:15:59 +0000

    Alphabet checker

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,11 +3,12 @@
 /**
  * _isalpha - Checks for alphabetic character
  * @c: The character to check
+ *
  * Return: 1 if c is alphabetic character, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')||((c >= 'A' && c<= 'Z')))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
