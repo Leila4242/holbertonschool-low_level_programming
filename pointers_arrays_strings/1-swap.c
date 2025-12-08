0d178403b06458e72b267f87a0593c509d9bec7a
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-08 12:15:31 +0000

    swapping values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets value to 98
+ * @n: pointer to an integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b){
+    	*a = b;
+        *b = a;
+}
