964928cea8af4aa9ff00a76a2760d3c9f6543819
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-08 11:19:36 +0000

    Resets value to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * Resets vaue to 98
- * @n: pointer
+ * reset_to_98 - Resets value to 98
+ * @n: pointer to an integer
  *
  * Return: void
  */
 
-void reset_to_98(int *n){
-    *n = 98;
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
