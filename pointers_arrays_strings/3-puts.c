18048313c537d58249111f575c4bcb5d41870613
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-08 12:47:12 +0000

    printing in new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+    _putchar("\n")
+	return 0;
+}
