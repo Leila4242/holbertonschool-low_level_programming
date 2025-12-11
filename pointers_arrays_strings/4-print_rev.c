3f9793cab49b482534dc9f8953c902899adf9967
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-11 08:31:41 +0000

    Reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,25 +3,25 @@
 
 /**
  * print_rev - prints a  reverse string, followed by a new line, to stdout
- * @str: pointer to the string to print
+ * @s: pointer to the string to print
  *
  * Return: void
  */
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		write(1, &str[i], 1);
+		write(1, &s[i], 1);
 		i++;
 	}
 	i--;
 
 	while (i>=0)
 	{
-		write(1, &str[i], 1);
+		write(1, &s[i], 1);
 		i--;
 	}
 	write(1, "\n", 1);
