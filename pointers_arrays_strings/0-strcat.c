53e75116cbbc8418e58fa155507040d3a9794d30
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-14 16:46:29 +0000

    concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string. Must be large enough to hold the result.
+ * @src: The source string to be appended.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
