6c424dcba82aa6cde3f8b6577e84fe567b1c1233
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-15 17:41:00 +0000

    Puts function recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		write(1, s, 1);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		write(1, "\n", 1);
+	}
 }
