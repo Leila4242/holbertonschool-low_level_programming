58471b654985f7c1f10cd30759b58079e370db7c
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-18 12:58:39 +0000

    Number of words

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints the number of the strings in line
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 int main(int argc,char* argv[])
