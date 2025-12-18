f3bcb76277772d8c66226ef235af223bce388532
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-18 09:45:22 +0000

    Factorial number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -7,15 +7,12 @@
  * Return: factorial of the number 
  */
 int factorial(int n)
-{
-    if(n > 0)
-    {
-        return(n*factorial(n-1));
-    }
-    else if(n==0) {
-        return(0);
+{      
+    if(n==0) {
+        return(1);
     }
     else{
         return(-1);
     }
+    return(n*factorial(n-1));
 }
