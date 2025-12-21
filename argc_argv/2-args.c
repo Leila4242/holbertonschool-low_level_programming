4beef0f6fd59ea2ff6a8fcb3ddc88edbc0e539c0
Leila4242 <aliyevaleyla6277@gmail.com>
2025-12-21 18:43:15 +0000

    args

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into the program
+ * main - prints all arguments it receives
  * @argc: argument count
- * @argv: argument vector (unused)
+ * @argv: argument vector
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void)argv; /* Mark argv as unused to avoid compiler warnings */
+	int i;
 
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
