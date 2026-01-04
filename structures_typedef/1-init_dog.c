a151eeb3f71b3ff9291370fd4ece6386689edfea
Leila4242 <aliyevaleyla6277@gmail.com>
2026-01-04 16:31:21 +0000

    Sturucture initializer

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,17 +1,19 @@
+#include <stddef.h>
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
+ * init_dog - initializes a struct dog
+ * @d: pointer to struct dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: void
+ * @owner: owner name
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
